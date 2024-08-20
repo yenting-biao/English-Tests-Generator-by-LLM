79c6c3560d799ea1b432b8dc75d095424d76735d
biao <91837449+yenting-biao@users.noreply.github.com>
2024-08-21 01:06:06 +0800

    fix(db): prevent too many connections by a method found on github

--- web/src/db/index.ts
+++ web/src/db/index.ts
@@ -3,26 +3,34 @@ import mysql from "mysql2/promise";
 import { privateEnv } from "@/lib/validators/env";
 import * as schema from "./schema";
 
-const connection = await mysql.createConnection({
+// https://github.com/drizzle-team/drizzle-orm/issues/1988
+declare global {
+  // eslint-disable-next-line no-var
+  var _db: ReturnType<typeof drizzle> | undefined;
+  // eslint-disable-next-line no-var
+  var _studentdb: mysql.Pool | undefined;
+}
+
+const connection = mysql.createPool({
   host: privateEnv.MYSQL_HOST,
   user: privateEnv.MYSQL_USER,
   password: privateEnv.MYSQL_PASSWORD,
   database: privateEnv.MYSQL_DB,
+  connectionLimit: 10,
 });
+const db = globalThis._db || drizzle(connection, { schema, mode: "default" });
 
-export const db = drizzle(connection, { schema, mode: "default" });
-
-export const studentdb = await mysql.createConnection({
+const studentdbConnection = mysql.createPool({
   host: privateEnv.MYSQL_HOST,
   user: privateEnv.MYSQL_USER,
   password: privateEnv.MYSQL_PASSWORD,
   database: privateEnv.MYSQL_STUDENT_DB,
 });
+const studentdb = globalThis._studentdb || studentdbConnection;
 
-// const client = new Client({
-//   connectionString: privateEnv.POSTGRES_URL,
-//   connectionTimeoutMillis: 5000,
-// });
+if (process.env.NODE_ENV !== "production") {
+  globalThis._db = db;
+  globalThis._studentdb = studentdb;
+}
 
-// await client.connect();
-// export const db = drizzle(client, { schema });
+export { db, studentdb };
