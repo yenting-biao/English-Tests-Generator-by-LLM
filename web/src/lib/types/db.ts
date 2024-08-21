bebee93db6b0ab98bc83e0502dc66ed24e0f10b7
biao <91837449+yenting-biao@users.noreply.github.com>
2024-08-22 00:13:48 +0800

    chore: the same thing done together with last submit

--- web/src/lib/types/db.ts
+++ web/src/lib/types/db.ts
@@ -4,3 +4,18 @@ export interface StudentUser {
   username: string;
   password: string;
 }
+
+export interface ClassInfo {
+  id: number;
+  name: string;
+  open: number;
+}
+
+export interface assignedTestsTableType {
+  testId: string;
+  id: string;
+  classNumber: number;
+  showAnswers: boolean;
+  startDate: Date;
+  endDate: Date;
+}
