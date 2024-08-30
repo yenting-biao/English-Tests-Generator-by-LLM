0aa5e4a75d43f32a2d59c3e1b68bf83b93163dc7
biao <91837449+yenting-biao@users.noreply.github.com>
2024-08-30 22:09:06 +0800

    fix(middleware): prevent student user access admin routes
    it is done by redirecting them to student's page.

--- web/src/middleware.ts
+++ web/src/middleware.ts
@@ -5,27 +5,39 @@ import { privateEnv } from "./lib/validators/env";
 export async function middleware(request: NextRequest) {
   const session = await auth();
 
-  if (
-    request.nextUrl.pathname.startsWith("/admin") &&
-    (!session || session?.user.username !== privateEnv.ADMIN_USERNAME)
-  ) {
-    return NextResponse.rewrite(new URL("/admin/login", request.url));
-  } else if (
+  // frontend admin routes
+  if (request.nextUrl.pathname.startsWith("/admin")) {
+    if (!session) {
+      return NextResponse.rewrite(new URL("/admin/login", request.url));
+    } else if (session?.user.username !== privateEnv.ADMIN_USERNAME) {
+      return NextResponse.rewrite(new URL("/student", request.url));
+    }
+  }
+  // frontend student routes
+  else if (
     request.nextUrl.pathname.startsWith("/student") &&
     (!session || !session.user)
   ) {
     return NextResponse.rewrite(new URL("/student/login", request.url));
-  } else if (request.nextUrl.pathname.startsWith("/api")) {
+  }
+  // api routes
+  else if (request.nextUrl.pathname.startsWith("/api")) {
+    // student api routes
     if (request.nextUrl.pathname.startsWith("/api/student")) {
       if (!session || !session.user) {
-        return NextResponse.rewrite(new URL("/student/login", request.url));
-      }
-    } else if (!request.nextUrl.pathname.startsWith("/api/auth")) {
-      if (!session || session?.user.username !== privateEnv.ADMIN_USERNAME) {
-        return NextResponse.rewrite(new URL("/admin/login", request.url));
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
       }
     }
-  } else if (!session || !session.user) {
+    // admin api routes
+    // /api/auth should not be protected, or else we will have a redirect loop
+    else if (!request.nextUrl.pathname.startsWith("/api/auth")) {
+      if (!session || session?.user.username !== privateEnv.ADMIN_USERNAME) {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+      }
+    }
+  }
+  // other cases
+  else if (!session || !session.user) {
     return NextResponse.rewrite(new URL("/student/login", request.url));
   }
 }
