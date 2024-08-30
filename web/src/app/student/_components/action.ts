c01058c6d9a216e9227aa4f3bfd9ec6e3819c609
biao <91837449+yenting-biao@users.noreply.github.com>
2024-08-30 22:09:39 +0800

    fix(student): fix the bug if class name is not found for that user's class number

--- web/src/app/student/_components/action.ts
+++ web/src/app/student/_components/action.ts
@@ -2,10 +2,12 @@ import { studentdb } from "@/db";
 
 export async function getClassName(classNumber: number) {
   "use server";
+  if (classNumber === 0) return "Class ADMIN";
   const [result] = await studentdb.query(
     "SELECT name FROM `FreshmanEnglish_group` WHERE `id` = ?",
     [classNumber]
   );
+  if (!result) return "Unknown Class";
   const className = (result as { name: string }[])[0].name;
   return className;
 }
